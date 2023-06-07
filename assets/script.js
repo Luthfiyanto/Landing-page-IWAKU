d038747a9999aa73e9a351e5ec1dc00a0ae45946
Luthfiyanto <89437612+Luthfiyanto@users.noreply.github.com>
2023-06-07 19:20:42 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function scrolll(){
+    let left = document.querySelector('.scrolllabel');
+    left.scrollBy(-300, 0);
+}
+
+function scrollr(){
+    let left = document.querySelector('.scrolllabel');
+    left.scrollBy(330, 0);
+}
+
+function no(){
+    alert("Mohon maaf, Fitur ini belum tersedia!")
+}
